116f0c8404b6a63076c3e55b167cdf35cc5829ff
PON-2003 <norati.m@psru.ac.th>
2025-06-20 11:52:14 +0700

    update user model/controller/routes and add gender field

--- models/userModel.js
+++ models/userModel.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-  firstname: { type: String, required: true },
-  lastname: { type: String, required: true },
-  email: { type: String, required: true },
-  phone: { type: String, required: true },
-  image: { type: String }, // เก็บ URL หรือ path ของรูปภาพ
-  gender: { type: String, required: true } // เพิ่มเพศ
+  firstname: { type: String, default: null },
+  lastname: { type: String, default: null },
+  email: { type: String, default: null },
+  phone: { type: String, default: null },
+  image: { type: String, default: null },
+  gender: { type: String, default: null }
 }, { timestamps: true, versionKey: false });
 
 module.exports = mongoose.model('User', UserSchema);
